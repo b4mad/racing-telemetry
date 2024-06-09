fc635ea5e1324850eb01770890ac1e37d2247767
Marcel Hild <hild@b4mad.net>
2024-06-09 17:57:09 +0200

    Refactored GraphQL query to retrieve all telemetry games with their names.
    Signed-off-by: Marcel Hild <hild@b4mad.net>

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -2,6 +2,7 @@ import unittest
 from telemetry import Telemetry
 from telemetry.retrieval.retrieval_strategy import RetrievalStrategy
 from telemetry.adapter.adapter import Adapter
+from telemetry.retrieval.graphql_retrieval_strategy import GraphQLRetrievalStrategy
 
 class MockDataRetrievalStrategy(RetrievalStrategy):
     def retrieve_data(self, filters):
@@ -30,6 +31,14 @@ class TestTelemetry(unittest.TestCase):
         telemetry.set_filter("key", "value")
         data = telemetry.get_data(adapter)
         self.assertEqual(data, [{"key": "value"}])
+
+    def test_graphql_retrieval_strategy(self):
+        strategy = GraphQLRetrievalStrategy(endpoint="http://telemetry.b4mad.racing:30050/graphql")
+        telemetry = Telemetry(strategy)
+        telemetry.set_filter("game", "example_game")
+        adapter = MockDataAdapter()
+        data = telemetry.get_data(adapter)
+        self.assertIsInstance(data, list)
 
 if __name__ == '__main__':
     unittest.main()
