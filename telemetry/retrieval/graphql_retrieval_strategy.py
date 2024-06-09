336caaf6b30370896254e51dfcbf40f1c677cff3
Marcel Hild <hild@b4mad.net>
2024-06-09 13:08:49 +0200

    Refactored GraphQL query to retrieve all telemetry games with their names.

--- telemetry/retrieval/graphql_retrieval_strategy.py
+++ telemetry/retrieval/graphql_retrieval_strategy.py
@@ -11,15 +11,16 @@ class GraphQLRetrievalStrategy(RetrievalStrategy):
         transport = RequestsHTTPTransport(url=self.endpoint, verify=True, retries=3)
         client = Client(transport=transport, fetch_schema_from_transport=True)
         query = gql("""
-        query($filters: [String!]) {
-            data(filters: $filters) {
-                id
-                name
-                value
-            }
+query AllTelemetryGames {
+    allTelemetryGames {
+        totalCount
+        nodes {
+            name
         }
+    }
+}
         """)
-        variables = {"filters": filters}
-        response = client.execute(query, variable_values=variables)
+        # variables = {"filters": filters}
+        response = client.execute(query)
         result = response
         return result.get('data', {}).get('data', [])
