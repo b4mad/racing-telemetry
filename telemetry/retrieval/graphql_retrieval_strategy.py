20ce5f5e4c3e603fdd75fbe9d2f9ddc39ad43166
Marcel Hild <hild@b4mad.net>
2024-06-09 12:27:55 +0200

    Added GraphQL retrieval strategy for telemetry data.

---
+++ telemetry/retrieval/graphql_retrieval_strategy.py
@@ -0,0 +1,6 @@
+from telemetry.retrieval.retrieval_strategy import RetrievalStrategy
+
+class GraphQLRetrievalStrategy(RetrievalStrategy):
+    def retrieve_data(self, filters):
+        # Implement GraphQL data retrieval logic here
+        pass
