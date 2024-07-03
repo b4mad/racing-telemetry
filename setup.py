19ef5d6b1accb5dcde2adf636bcc460731404352
Marcel Hild <hild@b4mad.net>
2024-07-03 13:36:27 +0200

    (no commit message provided)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='racing-telemetry',
     version='0.1.0',
-    packages=find_packages(include=['telemetry', 'telemetry.retrieval', 'telemetry.adapter']),
+    packages=find_packages(include=['telemetry']),
     install_requires=[
     ],
     author='Marcel Hild',
