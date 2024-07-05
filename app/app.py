bfef014d81600673aebeeb07f36828abdaaf19b0
Marcel Hild <hild@b4mad.net>
2024-07-05 12:00:21 +0200

    chore: Update views to synchronize x-axis range
    Signed-off-by: Marcel Hild <hild@b4mad.net>

--- app/app.py
+++ app/app.py
@@ -27,8 +27,12 @@ app.layout = html.Div([
             dcc.Graph(id='map-view', style={'height': '100%'})
         ], style={'width': '50%', 'height': '100%', 'display': 'inline-block'}),
         html.Div([
-            dcc.Graph(id='speed-view', style={'height': '50%'}),
-            dcc.Graph(id='throttle-view', style={'height': '50%'})
+            dcc.Graph(id='speed-view', style={'height': '16.67%'}),
+            dcc.Graph(id='throttle-view', style={'height': '16.67%'}),
+            dcc.Graph(id='brake-view', style={'height': '16.67%'}),
+            dcc.Graph(id='gear-view', style={'height': '16.67%'}),
+            dcc.Graph(id='steer-view', style={'height': '16.67%'}),
+            dcc.Graph(id='time-view', style={'height': '16.67%'})
         ], style={'width': '50%', 'height': '100%', 'display': 'inline-block', 'flexDirection': 'column'})
     ], style={'display': 'flex', 'height': '100vh'}),
     dcc.Store(id='shared-range')
@@ -76,33 +80,47 @@ def update_map_view(_):
 @app.callback(
     [Output('speed-view', 'figure'),
      Output('throttle-view', 'figure'),
+     Output('brake-view', 'figure'),
+     Output('gear-view', 'figure'),
+     Output('steer-view', 'figure'),
+     Output('time-view', 'figure'),
      Output('shared-range', 'data')],
     [Input('speed-view', 'relayoutData'),
-     Input('throttle-view', 'relayoutData')],
+     Input('throttle-view', 'relayoutData'),
+     Input('brake-view', 'relayoutData'),
+     Input('gear-view', 'relayoutData'),
+     Input('steer-view', 'relayoutData'),
+     Input('time-view', 'relayoutData')],
     [State('shared-range', 'data')]
 )
-def update_views(speed_relayout, throttle_relayout, shared_range):
+def update_views(speed_relayout, throttle_relayout, brake_relayout, gear_relayout, steer_relayout, time_relayout, shared_range):
     ctx = dash.callback_context
     trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-    if trigger_id == 'speed-view':
-        relayout_data = speed_relayout
-    elif trigger_id == 'throttle-view':
-        relayout_data = throttle_relayout
-    else:
-        relayout_data = None
+    relayout_data = None
+    for view, data in zip(['speed-view', 'throttle-view', 'brake-view', 'gear-view', 'steer-view', 'time-view'],
+                          [speed_relayout, throttle_relayout, brake_relayout, gear_relayout, steer_relayout, time_relayout]):
+        if trigger_id == view:
+            relayout_data = data
+            break
 
     if relayout_data and 'xaxis.range[0]' in relayout_data and 'xaxis.range[1]' in relayout_data:
         shared_range = [relayout_data['xaxis.range[0]'], relayout_data['xaxis.range[1]']]
     
-    speed_fig = lap_fig(df, columns=["SpeedMs"], title="SpeedMs")
+    speed_fig = lap_fig(df, columns=["SpeedMs"], title="Speed (m/s)")
     throttle_fig = lap_fig(df, columns=["Throttle"], title="Throttle")
+    brake_fig = lap_fig(df, columns=["Brake"], title="Brake")
+    gear_fig = lap_fig(df, columns=["Gear"], title="Gear")
+    steer_fig = lap_fig(df, columns=["SteeringAngle"], title="Steering Angle")
+    time_fig = lap_fig(df, columns=["CurrentLapTime"], title="Lap Time")
+
+    figures = [speed_fig, throttle_fig, brake_fig, gear_fig, steer_fig, time_fig]
 
     if shared_range:
-        speed_fig.update_xaxes(range=shared_range)
-        throttle_fig.update_xaxes(range=shared_range)
+        for fig in figures:
+            fig.update_xaxes(range=shared_range)
 
-    return speed_fig, throttle_fig, shared_range
+    return *figures, shared_range
 
 if __name__ == '__main__':
     app.run_server(debug=True)
